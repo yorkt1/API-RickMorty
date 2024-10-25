8e6c4b800961db6bb1bdf484cc4803477cc410f8
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-10-25 19:01:16 -0300

    chamei API e adicionei inner.HTML

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-const app = EventSource
+const charsContainer = document.querySelector ('chars-container');
+const API = 'https://rickandmortyapi.com/api';
+
+
+async function Characters({name, species, gender, status, page = 1}) {
+    
+    const response = await fettch(`'#{API}/caracter?name=${name}&species=${species}&status=${status}&gender={}${gender}&page=${page}'`)
+
+    const characters = await response.json()
+
+
+    return characters.result
+}
+
+async function render({characters}) {
+    characters.forEach((character) => {
+
+        return charsContainer.innerHTML += `
+            <div class="char">
+            <img src="${character.imagem}" alt="">
+            <div class="char-info">
+            <h3>Rick Sanchez</h3>
+            <span>Humano</span>
+            </div>`
+        
+    });
+    
+}
+
+render ()
